4b46ece3113b8ed616a810454603a941bda31cf5
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-08-13 22:01:41 +0300

    done

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,11 @@
 import FriendListItem from "../FriendListItem/FriendListItem";
+import css from "./FriendList.module.css";
 
 export default function FriendList({ friends }) {
   return (
-    <ul>
+    <ul className={css.list}>
       {friends.map((friend) => (
-        <li>
+        <li className={css.item} key={friend.id}>
           <FriendListItem friends={friend} />
         </li>
       ))}
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,16 +4,13 @@ import css from "./FriendListItem.module.css";
 export default function FriendListItem({
   friends: { isOnline, name, avatar },
 }) {
-  const containerClsx = clsx(
-    css.container,
-    isOnline ? css.isOnline : css.isRetired
-  );
+  const containerClsx = clsx(isOnline ? css.isOnline : css.isRetired);
 
   return (
-    <li className={containerClsx}>
+    <div className={css.container}>
       <img src={avatar} alt={name} width="48" />
       <p>{name}</p>
-      <p>{isOnline ? "Online" : "Offline"}</p>
-    </li>
+      <p className={containerClsx}>{isOnline ? "Online" : "Offline"}</p>
+    </div>
   );
 }
